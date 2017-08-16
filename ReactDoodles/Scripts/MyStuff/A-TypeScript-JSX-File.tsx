f40e6192c7d69ea41f7e650a95b114b533824095
Collin Kruger <collin.kruger@live.com>
2017-08-16 12:31:11 -0500

    Init

---
+++ ReactDoodles/Scripts/MyStuff/A-TypeScript-JSX-File.tsx
@@ -0,0 +1,11 @@
+﻿/// <reference path="../typings/react.d.ts" />
+/// <reference path="../typings/react-dom.d.ts" />
+
+class AnExampleComponent extends React.Component<any, any> {
+    render() {
+        return <p>Hello React World!</p>
+    }
+}
+
+
+ReactDOM.render(<AnExampleComponent />, document.getElementById("root"));
